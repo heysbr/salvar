c68dcd8f214a180daa320ca1c1260e7051dc3685
Priyanshu Sabaar <priyanshu.tca2263106@tmu.ac.in>
2025-07-18 12:53:55 +0530

    feat: add Demo component and update main entry point to render it

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import Demo from "./components/Demo.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Demo/>
   </StrictMode>
 );
